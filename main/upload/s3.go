6dbb3e5514fcc6b7d5c9bc91ccca7155e97cfe8a
afternun <michal.kacper.balos@gmail.com>
2023-09-06 21:13:25 +0200

    basic dl/up

--- main/upload/s3.go
+++ main/upload/s3.go
@@ -3,28 +3,62 @@ package upload
 import (
 	"buckmate/main/common/exception"
 	"buckmate/structs"
+	"context"
+	"log"
 	"os"
+	"path/filepath"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func S3 (paths string[], bucket string) {
-	sess, err := session.Must(session.NewSession())
-	exception.Handle(structs.Exception{Err: err, Message: "Couldn't create AWS session."})
+var (
+	LocalDirectory = "build"
+)
 
-	uploader := s3manager.NewUploader(sess)
+func S3(bucket string, prefix string) {
+	walker := make(fileWalk)
+	go func() {
+		// Gather the files to upload by walking the path recursively
+		if err := filepath.Walk(LocalDirectory, walker.Walk); err != nil {
+			log.Fatalln("Walk failed:", err)
+		}
+		close(walker)
+	}()
 
-	for _, path range paths {
-		file, err := os.OpenFile(path)
-		exception.Handle(structs.Exception{Err: err, Message: "Couldn't open file for upload."})
-		result, err := uploader.Upload(&s3manager.UploadInput{
-			Bucket: aws.String(bucket)
-			Key: aws.String(path)
-			Body: file
+	cfg, cfgErr := config.LoadDefaultConfig(context.TODO())
+	exception.Handle(structs.Exception{Err: cfgErr, Message: "Couldn't get config."})
+
+	// For each file found walking, upload it to Amazon S3
+	uploader := manager.NewUploader(s3.NewFromConfig(cfg))
+	for path := range walker {
+		rel, err := filepath.Rel(LocalDirectory, path)
+		exception.Handle(structs.Exception{Err: err, Message: "Couldn't get relative path."})
+
+		file, err := os.Open(path)
+		exception.Handle(structs.Exception{Err: err, Message: "Failed to open file"})
+
+		defer file.Close()
+		result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+			Bucket: &bucket,
+			Key:    aws.String(filepath.Join(prefix, rel)),
+			Body:   file,
 		})
+		exception.Handle(structs.Exception{Err: err, Message: "Failed to upload"})
+		log.Println("Uploaded", path, result.Location)
 	}
+}
 
+type fileWalk chan string
 
+func (f fileWalk) Walk(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() {
+		f <- path
+	}
+	return nil
 }
