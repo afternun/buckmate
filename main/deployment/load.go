4fde7d2b487fcb63b953a21efa4b43c30f14f6fe
Michał Bałos <baawhee@Michas-MacBook-Pro.local>
2023-11-28 20:15:38 +0100

    one config file, remove exceptions abstraction

--- main/deployment/load.go
+++ main/deployment/load.go
@@ -1,19 +1,39 @@
 package deployment
 
 import (
-	"buckmate/main/common/exception"
 	"buckmate/main/common/util"
 	"buckmate/structs"
+	"log"
 )
 
-func Load() structs.Deployment {
-	deploymentRaw, deploymentRawErr := util.LoadYaml("buckmate/Deployment.yaml")
-	exception.Handle(structs.Exception{Err: deploymentRawErr, Message: "Loading deployment failed."})
+func Load(env string) structs.Deployment {
+	commonFile, err := util.LoadYaml("buckmate/Deployment.yaml")
+	if err != nil {
+		log.Fatalln("Loading common config failed")
+	}
 
-	deployment := structs.Deployment{}
+	envFile, err := util.LoadYaml("buckmate/" + env + "/Deployment.yaml")
+	if err != nil {
+		log.Fatalln("Loading environment config failed")
+	}
 
-	errDeploymentYaml := util.YamlToStruct(deploymentRaw, &deployment)
-	exception.Handle(structs.Exception{Err: errDeploymentYaml, Message: "Unmarshaling deployment failed."})
+	commonConfig := structs.Deployment{}
+	envConfig := structs.Deployment{}
 
-	return deployment
+	err2 := util.YamlToStruct(commonFile, &commonConfig)
+	if err2 != nil {
+		log.Fatalln("Unmarshaling common config failed.")
+	}
+
+	err3 := util.YamlToStruct(envFile, &envConfig)
+	if err3 != nil {
+		log.Fatalln("Unmarshaling environment config failed.")
+	}
+
+	err4 := util.MergeStruct(&commonConfig, envConfig)
+	if err4 != nil {
+		log.Fatalln("Merging configs failed")
+	}
+
+	return commonConfig
 }
--- structs/exception.go
+++
@@ -1,7 +0,0 @@
-package structs
-
-type Exception struct {
-	Err      error
-	Message  string
-	ExitCode int
-}
