ba623631241b4dec9ab19c383b8da0c7d637656d
afternun <michal.kacper.balos@gmail.com>
2024-01-21 21:29:21 +0100

    fix log flag

--- cmd/root.go
+++ cmd/root.go
@@ -6,22 +6,26 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "buckmate",
-	Short: "Deploy to S3 buckets with ease",
-	Long:  ``,
+	Use:     "buckmate",
+	Short:   "Deploy to S3 buckets with ease",
+	Long:    ``,
+	Version: "0.0.1",
+	Run: func(cmd *cobra.Command, args []string) {
+		logLevel, err := cmd.Flags().GetString("log")
+		if err != nil {
+			log.Fatal(err)
+		}
+		parsedLogLevel, err := log.ParseLevel(logLevel)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.SetLevel(parsedLogLevel)
+		cmd.Help()
+	},
 }
 
 func Execute() {
-	logLevel, err := rootCmd.Flags().GetString("log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	parsedLogLevel, err := log.ParseLevel(logLevel)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.SetLevel(parsedLogLevel)
-	err = rootCmd.Execute()
+	err := rootCmd.Execute()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,5 +34,5 @@ func Execute() {
 func init() {
 	rootCmd.PersistentFlags().StringP("env", "e", "", "Specifies which config to apply - directory name that contains environment specific Config.yaml and files to be copied.")
 	rootCmd.PersistentFlags().StringP("path", "p", "", "Specifies path to the directory that contains buckmate directory with Deployment.yaml config.")
-	rootCmd.PersistentFlags().StringP("log", "l", "InfoLevel", "Specifies log level. TraceLevel, DebugLevel, InfoLevel, WarnLevel, ErrorLevel, FatalLevel, PanicLevel.")
+	rootCmd.PersistentFlags().StringP("log", "l", "info", "Specifies log level. Options: panic, fatal, error, warn, info, debug trace.")
 }
